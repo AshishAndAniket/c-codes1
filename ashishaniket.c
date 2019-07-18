c78d17d4c3ef83e8d05975261b4c6b1b480dba34
AshishAndAniket <53028836+AshishAndAniket@users.noreply.github.com>
2019-07-18 09:28:57 +0530

    Update ashishaniket.c

--- ashishaniket.c
+++ ashishaniket.c
@@ -3,13 +3,15 @@ void main()
 {
 printf("welcome to horizon 2k19");
 printf("Let do simple addition and subraction code in c");
-int a,b,c,d;
+int a,b,c,d,e;
 printf("Enter value of a:");
 scanf("%d",&a);
 printf("Enter value of b:");
 scanf("%d",&b);
 c=a+b;
 d=a-b;
+e=a*b;
 printf("The addition of two numbers is:%d",c);
 printf("The difference of two numbers is:%d",d);
+printf("Multiplication is"%d",e); 
 }
